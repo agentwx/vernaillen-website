6a9912ce4fee7f8c771db6b1e259bf1a976b8bc8
Wouter Vernaillen <wouter@triberay.com>
2019-07-11 14:05:42 +0200

    Breadcrumb component

---
+++ src/components/Breadcrumbs.vue
@@ -0,0 +1,16 @@
+<template>
+  <div padding>
+    <q-breadcrumbs>
+      <q-breadcrumbs-el label="Home" icon="home" to="/home" />
+      <q-breadcrumbs-el :label="$store.state.common.currentPageName" />
+    </q-breadcrumbs>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+</style>
